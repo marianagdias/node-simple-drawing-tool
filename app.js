b5df0dcb7012182eb36d11b424e92d5661af2a7c
Mariana Galante Dias <102020878+marianagdias@users.noreply.github.com>
2022-08-26 20:27:02 +0100

    Add creating horizontal line

--- app.js
+++ app.js
@@ -8,17 +8,71 @@ const input = [
   { command: "B", x: 10, y: 3, color: "o" },
 ];
 
-input.forEach((inputs, index) => {
-  if (inputs.command === "C" && index === 0) {
-    const dash = "-";
-    console.log(dash.repeat(inputs.width + 2));
-    const space = " ";
-    if (inputs.heigth >= 0) {
-      for (var i = 0; i < inputs.heigth; i++) {
-        console.log("|" + space.repeat(inputs.width) + "|");
+const dash = "-";
+const space = " ";
+const dot = "x";
+
+function createCanvas() {
+  input.forEach((inputs, index) => {
+    if (inputs.command === "C" && index === 0) {
+      console.log(dash.repeat(inputs.width + 2));
+
+      if (inputs.heigth >= 0) {
+        for (let i = 0; i < inputs.heigth; i++) {
+          console.log("|" + space.repeat(inputs.width) + "|");
+        }
+      }
+      console.log(dash.repeat(inputs.width + 2));
+    }
+  });
+}
+
+createCanvas();
+
+function draw() {
+  input.forEach((inputs, index) => {
+    if (index >= 1 && input[0].command === "C") {
+      if (inputs.command === "L") {
+        if (inputs.y1 === inputs.y2) {
+          console.log(dash.repeat(input[0].width + 2));
+
+          for (let i = 1; i < inputs.y1; i++) {
+            console.log("|" + space.repeat(input[0].width) + "|");
+          }
+
+          let horizontalLine =
+            "|" +
+            space.repeat(inputs.x1 - 1) +
+            dot.repeat(inputs.x2 - inputs.x1) +
+            space.repeat(input[0].width - inputs.x2 + 1) +
+            "|";
+          console.log(horizontalLine);
+
+          for (let i = inputs.y1; i < input[0].heigth; i++) {
+            console.log("|" + space.repeat(input[0].width) + "|");
+          }
+          //   for (let i = 0; i < input[0].heigth; i++) {
+          //     console.log("|" + space.repeat(input[0].width) + "|");
+          //   }
+
+          //   if (inputs.heigth >= 0) {
+          //     for (var i = 0; i < inputs.heigth; i++) {
+          //       console.log("|" + space.repeat(input[0].width) + "|");
+          //     }
+          //   }
+          console.log(dash.repeat(input[0].width + 2));
+        }
+        if (inputs.x1 === inputs.x2) {
+        }
+      }
+
+      if (inputs.command === "R") {
+      }
+
+      if (inputs.command === "B") {
       }
     }
-    console.log(dash.repeat(inputs.width + 2));
-  }
-  console.log();
-});
+  });
+}
+
+draw();
