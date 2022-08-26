3981943b7359acb02a12d6c101286e2974ce7516
Mariana Galante Dias <102020878+marianagdias@users.noreply.github.com>
2022-08-26 19:27:10 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const input = [
+  { command: "C", width: 20, heigth: 4 },
+  { command: "L", x1: 1, y1: 2, x2: 6, y2: 2 },
+  { command: "L", x1: 6, y1: 3, x2: 6, y2: 4 },
+  { command: "R", x1: 16, y1: 1, x2: 20, y2: 3 },
+  { command: "B", x: 10, y: 3, color: "o" },
+];
