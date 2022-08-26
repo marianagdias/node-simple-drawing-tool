b2e0d3042bb5016fcb7fe6929bad5ee92139adff
Mariana Galante Dias <102020878+marianagdias@users.noreply.github.com>
2022-08-26 19:40:28 +0100

    Add creating canvas (fixed)

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ const input = [
   { command: "R", x1: 16, y1: 1, x2: 20, y2: 3 },
   { command: "B", x: 10, y: 3, color: "o" },
 ];
+
+input.forEach((inputs, index) => {
+  if (inputs.command === "C" && index === 0) {
+    const dash = "-";
+    console.log(dash.repeat(inputs.width + 2));
+    const space = " ";
+    if (inputs.heigth >= 0) {
+      for (var i = 0; i < inputs.heigth; i++) {
+        console.log("|" + space.repeat(inputs.width) + "|");
+      }
+    }
+    console.log(dash.repeat(inputs.width + 2));
+  }
+  console.log();
+});
