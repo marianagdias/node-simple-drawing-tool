80d540fb5ed4ed56ac69341c6d503e6aa7ec5242
Mariana Galante Dias <102020878+marianagdias@users.noreply.github.com>
2022-08-28 11:53:52 +0100

    Update app to take dynamic console inputs

--- app.js
+++ app.js
@@ -9,26 +9,17 @@ let consoleInput = [];
 
 console.log(consoleInput);
 
-const input = [
-  { command: "C", width: 20, heigth: 4 },
-  { command: "L", x1: 1, y1: 2, x2: 6, y2: 2 },
-  { command: "L", x1: 6, y1: 3, x2: 6, y2: 4 },
-  { command: "R", x1: 16, y1: 1, x2: 20, y2: 3 },
-  { command: "B", x: 10, y: 3, color: "o" },
-];
-
 const dash = "-";
 const space = " ";
 const dot = "x";
 let canvas = [];
-let upperLine = [];
-let bottomLine = [];
-let sides = [];
 
 function createCanvas() {
   consoleInput.forEach((inputs, index) => {
     if (inputs[0] === "C" && index === 0) {
-      let upperCanvas = dash.repeat(inputs[1] + 2);
+      console.log(inputs[1]);
+      let upperCanvas = dash.repeat(Number(inputs[1]) + 2);
+
       let splitUpperCanvas = upperCanvas.split("");
       canvas.push(splitUpperCanvas);
 
@@ -49,24 +40,22 @@ function createCanvas() {
 }
 
 function draw() {
-  input.forEach((inputs, index) => {
+  consoleInput.forEach((inputs, index) => {
     //IF CANVAS IS CREATED
-    if (index >= 1 && input[0].command === "C") {
+    if (index >= 1 && consoleInput[0][0] === "C") {
       //LINES
-      if (inputs.command === "L") {
+      if (inputs[0] === "L") {
         //HORIZONTAL LINE
-        if (inputs.y1 === inputs.y2) {
-          canvas[inputs.y1].splice(
-            inputs.x1,
-            inputs.x2 - inputs.x1 + 1,
-            dot.repeat(inputs.x2 - inputs.x1 + 1)
+        if (inputs[2] === inputs[4]) {
+          canvas[inputs[2]].splice(
+            inputs[1],
+            inputs[3] - inputs[1] + 1,
+            dot.repeat(inputs[3] - inputs[1] + 1)
           );
 
-          let joinCanvas = canvas[inputs.y1].join("");
-          canvas[inputs.y1] = joinCanvas;
-          canvas[inputs.y1] = canvas[inputs.y1].split("");
-
-          // canvas[inputs.y1].split("");
+          let joinCanvas = canvas[inputs[2]].join("");
+          canvas[inputs[2]] = joinCanvas;
+          canvas[inputs[2]] = canvas[inputs[2]].split("");
 
           canvas.forEach((outputs) => {
             console.log(outputs.join(""));
@@ -74,9 +63,9 @@ function draw() {
         }
 
         //VERTICAL LINE
-        if (inputs.x1 === inputs.x2) {
-          for (let i = inputs.y1; i <= inputs.y2; i++) {
-            canvas[i].splice(inputs.x1, 1, "x");
+        if (inputs[1] === inputs[3]) {
+          for (let i = inputs[2]; i <= inputs[4]; i++) {
+            canvas[i].splice(inputs[1], 1, "x");
           }
 
           canvas.forEach((outputs) => {
@@ -86,34 +75,34 @@ function draw() {
       }
 
       //RECTANGLE
-      if (inputs.command === "R") {
-        canvas[inputs.y1].splice(
-          inputs.x1,
-          inputs.x2 - inputs.x1 + 1,
-          "x".repeat(inputs.x2 - inputs.x1 + 1)
+      if (inputs[0] === "R") {
+        canvas[inputs[2]].splice(
+          inputs[1],
+          inputs[3] - inputs[1] + 1,
+          "x".repeat(inputs[3] - inputs[1] + 1)
         );
 
-        let joinCanvas = canvas[inputs.y1].join("");
-        canvas[inputs.y1] = joinCanvas;
-        canvas[inputs.y1] = canvas[inputs.y1].split("");
+        let joinCanvas = canvas[inputs[2]].join("");
+        canvas[Number(inputs[2])] = joinCanvas;
+        canvas[Number(inputs[2])] = canvas[inputs[2]].split("");
 
-        for (let i = inputs.y1 + 1; i < inputs.y2; i++) {
-          canvas[i].splice(inputs.x1, 1, "x");
+        for (let i = Number(inputs[2]) + 1; i < Number(inputs[4]); i++) {
+          canvas[i].splice(Number(inputs[1]), 1, "x");
         }
 
-        for (let i = inputs.y1 + 1; i < inputs.y2; i++) {
-          canvas[i].splice(inputs.x2, 1, "x");
+        for (let i = Number(inputs[2]) + 1; i < Number(inputs[4]); i++) {
+          canvas[i].splice(Number(inputs[3]), 1, "x");
         }
 
-        canvas[inputs.y2].splice(
-          inputs.x1,
-          inputs.x2 - inputs.x1 + 1,
-          "x".repeat(inputs.x2 - inputs.x1 + 1)
+        canvas[inputs[4]].splice(
+          inputs[1],
+          inputs[3] - inputs[1] + 1,
+          "x".repeat(inputs[3] - inputs[1] + 1)
         );
 
-        let joinCanvas2 = canvas[inputs.y2].join("");
-        canvas[inputs.y2] = joinCanvas2;
-        canvas[inputs.y2] = canvas[inputs.y2].split("");
+        let joinCanvas2 = canvas[inputs[4]].join("");
+        canvas[inputs[4]] = joinCanvas2;
+        canvas[inputs[4]] = canvas[inputs[4]].split("");
 
         canvas.forEach((outputs) => {
           console.log(outputs.join(""));
@@ -121,17 +110,14 @@ function draw() {
       }
 
       //BUCKET FILL
-      if (inputs.command === "B") {
+      if (inputs[0] === "B") {
         function bucketFill(x, y, oldColor, newColor) {
           if (x < 0 || x >= canvas[y].length || y < 0 || y >= canvas.length)
             return;
           if (canvas[y][x] !== oldColor) return;
 
-          // set the color of node to newColor
-
           canvas[y][x] = newColor;
 
-          //look for neighboring cell
           bucketFill(x + 1, y, oldColor, newColor);
           bucketFill(x - 1, y, oldColor, newColor);
           bucketFill(x, y + 1, oldColor, newColor);
@@ -139,11 +125,12 @@ function draw() {
         }
 
         bucketFill(
-          inputs.x,
-          inputs.y,
-          canvas[inputs.y][inputs.x],
-          inputs.color
+          Number(inputs[1]),
+          Number(inputs[2]),
+          canvas[inputs[2]][inputs[1]],
+          inputs[3]
         );
+        console.log(canvas);
 
         canvas.forEach((outputs) => {
           console.log(outputs.join(""));
@@ -159,6 +146,7 @@ function takeInput(input) {
     return;
   } else {
     consoleInput.push(input.split(" "));
+    draw();
     readline.question("Type another input or Q to finish \n", takeInput);
   }
 }
@@ -170,6 +158,3 @@ readline.question(`Type the input to create a canvas \n`, (canvasInput) => {
 
   return;
 });
-
-// createCanvas();
-// draw();
