4d7c50250d99a2fbe3a6180aabee064a67d45052
Mariana Galante Dias <102020878+marianagdias@users.noreply.github.com>
2022-08-28 07:28:14 +0100

    Update app to create dynamycally

--- app.js
+++ app.js
@@ -21,121 +21,131 @@ const input = [
 const dash = "-";
 const space = " ";
 const dot = "x";
+let canvas = [];
+let upperLine = [];
+let bottomLine = [];
+let sides = [];
 
 function createCanvas() {
   input.forEach((inputs, index) => {
     if (inputs.command === "C" && index === 0) {
-      console.log(dash.repeat(inputs.width + 2));
+      let upperCanvas = dash.repeat(inputs.width + 2);
+      let splitUpperCanvas = upperCanvas.split("");
+      canvas.push(splitUpperCanvas);
 
       if (inputs.heigth >= 0) {
         for (let i = 0; i < inputs.heigth; i++) {
-          console.log("|" + space.repeat(inputs.width) + "|");
+          let middleCanvas = "|" + space.repeat(inputs.width) + "|";
+          let splitMiddleCanvas = middleCanvas.split("");
+          canvas.push(splitMiddleCanvas);
         }
       }
-      console.log(dash.repeat(inputs.width + 2));
+      canvas.push(splitUpperCanvas);
+
+      canvas.forEach((outputs, index) => {
+        console.log(outputs.join(""));
+      });
+    }
+  });
+}
+
+function draw() {
+  input.forEach((inputs, index) => {
+    //IF CANVAS IS CREATED
+    if (index >= 1 && input[0].command === "C") {
+      //LINES
+      if (inputs.command === "L") {
+        //HORIZONTAL LINE
+        if (inputs.y1 === inputs.y2) {
+          canvas[inputs.y1].splice(
+            inputs.x1,
+            inputs.x2 - inputs.x1 + 1,
+            dot.repeat(inputs.x2 - inputs.x1 + 1)
+          );
+
+          let joinCanvas = canvas[inputs.y1].join("");
+          canvas[inputs.y1] = joinCanvas;
+          canvas[inputs.y1] = canvas[inputs.y1].split("");
+
+          // canvas[inputs.y1].split("");
+
+          canvas.forEach((outputs) => {
+            console.log(outputs.join(""));
+          });
+        }
+
+        //VERTICAL LINE
+        if (inputs.x1 === inputs.x2) {
+          for (let i = inputs.y1; i <= inputs.y2; i++) {
+            canvas[i].splice(inputs.x1, 1, "x");
+          }
+
+          canvas.forEach((outputs) => {
+            console.log(outputs.join(""));
+          });
+        }
+      }
+
+      //RECTANGLE
+      if (inputs.command === "R") {
+        canvas[inputs.y1].splice(
+          inputs.x1,
+          inputs.x2 - inputs.x1 + 1,
+          "x".repeat(inputs.x2 - inputs.x1 + 1)
+        );
+
+        let joinCanvas = canvas[inputs.y1].join("");
+        canvas[inputs.y1] = joinCanvas;
+        canvas[inputs.y1] = canvas[inputs.y1].split("");
+
+        for (let i = inputs.y1 + 1; i < inputs.y2; i++) {
+          canvas[i].splice(inputs.x1, 1, "x");
+        }
+
+        for (let i = inputs.y1 + 1; i < inputs.y2; i++) {
+          canvas[i].splice(inputs.x2, 1, "x");
+        }
+
+        canvas[inputs.y2].splice(
+          inputs.x1,
+          inputs.x2 - inputs.x1 + 1,
+          "x".repeat(inputs.x2 - inputs.x1 + 1)
+        );
+
+        let joinCanvas2 = canvas[inputs.y2].join("");
+        canvas[inputs.y2] = joinCanvas2;
+        canvas[inputs.y2] = canvas[inputs.y2].split("");
+
+        canvas.forEach((outputs) => {
+          console.log(outputs.join(""));
+        });
+      }
+
+      //BUCKET FILL
+      function bucketFill(i, j, oldColor, newColor) {
+        let canvasLine = "";
+        if (inputs.command === "B") {
+          //SPLIT LINES INTO ARRAYS
+
+          if (canvas[j] !== oldColor) return;
+          // set the color of node to newColor
+          canvas.forEach((element, index) => {
+            for (let i = 0; i <= index; i++) {
+              canvasLine = element;
+              canvasLine[x] = newColor;
+            }
+          });
+
+          bucketFill(i + 1, j, oldColor, newColor);
+          bucketFill(i - 1, j, oldColor, newColor);
+          bucketFill(i, j + 1, oldColor, newColor);
+          bucketFill(i, j - 1, oldColor, newColor);
+        }
+      }
+      bucketFill(inputs.x, inputs.y, " ", inputs.color);
     }
   });
 }
 
 createCanvas();
-
-function draw() {
-  input.forEach((inputs, index) => {
-    if (index >= 1 && input[0].command === "C") {
-      if (inputs.command === "L") {
-        if (inputs.y1 === inputs.y2) {
-          console.log(dash.repeat(input[0].width + 2));
-
-          for (let i = 1; i < inputs.y1; i++) {
-            console.log("|" + space.repeat(input[0].width) + "|");
-          }
-
-          let horizontalLine =
-            "|" +
-            space.repeat(inputs.x1 - 1) +
-            dot.repeat(inputs.x2 - inputs.x1 + 1) +
-            space.repeat(input[0].width - inputs.x2) +
-            "|";
-          console.log(horizontalLine);
-
-          for (let i = inputs.y1; i < input[0].heigth; i++) {
-            console.log("|" + space.repeat(input[0].width) + "|");
-          }
-
-          console.log(dash.repeat(input[0].width + 2));
-        }
-
-        if (inputs.x1 === inputs.x2) {
-          console.log(dash.repeat(input[0].width + 2));
-
-          for (let i = 1; i < inputs.y1; i++) {
-            console.log("|" + space.repeat(input[0].width) + "|");
-          }
-
-          for (i = inputs.y1; i <= inputs.y2; i++) {
-            console.log(
-              "|" +
-                space.repeat(inputs.x1 - 1) +
-                dot +
-                space.repeat(input[0].width - inputs.x2) +
-                "|"
-            );
-          }
-
-          for (let i = inputs.y2 + 1; i < input[0].heigth; i++) {
-            console.log("|" + space.repeat(input[0].width) + "|");
-          }
-
-          console.log(dash.repeat(input[0].width + 2));
-        }
-      }
-
-      if (inputs.command === "R") {
-        console.log(dash.repeat(input[0].width + 2));
-
-        for (let i = 1; i < inputs.y1; i++) {
-          console.log("|" + space.repeat(input[0].width) + "|");
-        }
-
-        console.log(
-          "|" +
-            space.repeat(inputs.x1 - 1) +
-            dot.repeat(inputs.x2 - inputs.x1 + 1) +
-            space.repeat(input[0].width - inputs.x2) +
-            "|"
-        );
-
-        for (let i = inputs.y1 + 1; i < inputs.y2; i++) {
-          console.log(
-            "|" +
-              space.repeat(inputs.x1 - 1) +
-              dot +
-              space.repeat(inputs.x2 - inputs.x1 - 1) +
-              dot +
-              space.repeat(input[0].width - inputs.x2) +
-              "|"
-          );
-        }
-
-        console.log(
-          "|" +
-            space.repeat(inputs.x1 - 1) +
-            dot.repeat(inputs.x2 - inputs.x1 + 1) +
-            space.repeat(input[0].width - inputs.x2) +
-            "|"
-        );
-
-        for (let i = inputs.y2; i < input[0].heigth; i++) {
-          console.log("|" + space.repeat(input[0].width) + "|");
-        }
-
-        console.log(dash.repeat(input[0].width + 2));
-      }
-
-      if (inputs.command === "B") {
-      }
-    }
-  });
-}
-
 draw();
